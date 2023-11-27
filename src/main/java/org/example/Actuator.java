dd64dd6dd5920233e708d72312d18a0cddaaaee0
Emil Kjær Fuglsang <emilkf95@gmail.com>
2023-11-27 10:46:54 +0100

    Added Actuator class

---
+++ src/main/java/org/example/Actuator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Actuator {
+    private float setPoint;
+    private String name;
+    private String type;
+    private Sensor sensor;
+
+    public Actuator(float setPoint, String name, String type, Sensor sensor) {
+        this.setPoint = setPoint;
+        this.name = name;
+        this.type = type;
+        this.sensor = sensor;
+    }
+
+    public float getSetPoint() {
+        return setPoint;
+    }
+
+    public void setSetPoint(float setPoint) {
+        this.setPoint = setPoint;
+    }
+
+    public boolean checkValue() {
+        return sensor.getValue() > this.setPoint;
+    }
+}
